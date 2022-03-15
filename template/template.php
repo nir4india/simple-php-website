f30500cfada31e50f273e5e148c9d722c2435749
nir4india <nir4india@gmail.com>
2022-03-15 15:55:11 +0530

    Update template.php

--- template/template.php
+++ template/template.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+    <meta http-equiv="x-ua-compatible" content="ie=edge"/
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <title><?php page_title(); ?> | <?php site_name(); ?></title>
     <link href="<?php site_url(); ?>/template/style.css" rel="stylesheet" type="text/css" /> 
